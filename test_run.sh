697eca30c676d16fdc4a8455b70dc17893f3a5e8
Dat T Nguyen <n.dat@outlook.com>
2023-09-12 13:20:08 +0200

    test run

--- test_run.sh
+++ test_run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --nodes=1                
+#SBATCH --job-name=cscQTL
+#SBATCH --mem=4G
+#SBATCH --partition=gpu                
+#SBATCH --mail-user=nguyen.thanh.dat@nmbu.no
+#SBATCH --mail-type=ALL
 
-cd /sigma4/projects/cscqtl_revise/simualtion/circall_quant
 
 export TOWER_ACCESS_TOKEN=eyJ0aWQiOiA3OTAxfS4xNGY5NTFmOWNiZmEwNjZhOGFkYzliZTg3MDc4YWI4ZTRiYTk4ZmI5
 
@@ -11,4 +17,4 @@ export TOWER_ACCESS_TOKEN=eyJ0aWQiOiA3OTAxfS4xNGY5NTFmOWNiZmEwNjZhOGFkYzliZTg3MD
 reads="/mnt/project/Aqua-Faang/dat/cscqtl/simulation/*_{1,2}.fq.gz"
 
 
-nextflow run main.nf -resume -with-tower --reads /mnt/project/Aqua-Faang/dat/cscqtl/simulation/*_{1,2}.fq.gz --circRNA $PWD/data/simulated_circRNA_list.txt
+nextflow run main.nf -resume -with-tower -profile cluster --reads /mnt/project/Aqua-Faang/dat/cscqtl/simulation/*_{1,2}.fq.gz --circRNA $PWD/data/simulated_circRNA_list.txt
