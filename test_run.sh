e6dd32ba927c561fc8d5cf32d69bffeaf2baf104
Dat T Nguyen <n.dat@outlook.com>
2023-09-12 13:18:21 +0200

    add test list circRNA

--- test_run.sh
+++ test_run.sh
@@ -8,13 +8,7 @@ export TOWER_ACCESS_TOKEN=eyJ0aWQiOiA3OTAxfS4xNGY5NTFmOWNiZmEwNjZhOGFkYzliZTg3MD
 #cd /mnt/project/Aqua-Faang/dat/cscqtl/tcell_cscQTL_dev
 
 
-reads="/sigma4/projects/cscqtl_revise/simualtion/*_{1,2}.fq.gz"
+reads="/mnt/project/Aqua-Faang/dat/cscqtl/simulation/*_{1,2}.fq.gz"
 
 
-nextflow run main.nf -resume -with-tower \
-    --reads "/sigma4/projects/cscqtl_revise/simualtion/*_{1,2}.fq.gz" \
-    --genome "/sigma4/data/genome_ref_GRCh37.75/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa" \
-    --cdna "/sigma4/data/genome_ref_GRCh37.75/Homo_sapiens.GRCh37.75.cdna.all.fa" \
-    --annotation "/sigma4/data/genome_ref_GRCh37.75/Homo_sapiens.GRCh37.75.gtf" \
-    --circRNA "" \
-
+nextflow run main.nf -resume -with-tower --reads /mnt/project/Aqua-Faang/dat/cscqtl/simulation/*_{1,2}.fq.gz --circRNA $PWD/data/simulated_circRNA_list.txt
